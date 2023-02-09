829ed25b83bceb971f33688a482f27e67af1ee96
r-s-i <77388911+r-s-i@users.noreply.github.com>
2023-02-09 13:29:13 +0100

    2 of out 13 user stories is fulfilled

--- script.js
+++ script.js
@@ -3,6 +3,23 @@ d3.select("body")
   .text("United States GDP, 1947-2015")
   .attr("id", "title");
 
+// Canvas:
+let svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", 500)
+  .attr("height", 500);
+
+// x-axis:
+let xScale = d3.scaleLinear().range([0, 400]).domain([0, 50]);
+let xAxis = d3.axisBottom(xScale).ticks(10);
+
+svg
+  .append("g")
+  .attr("transform", "translate(10," + 470 + ")")
+  .attr("id", "x-axis")
+  .call(xAxis);
+
 fetch(
   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json"
 )
