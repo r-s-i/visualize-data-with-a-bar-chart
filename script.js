09e70dd1c4f9cfa1ffc2202d6ca41781f145ebd3
r-s-i <77388911+r-s-i@users.noreply.github.com>
2023-02-09 09:35:08 +0100

    1 of out 13 user stories passes

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+d3.select("body")
+  .append("h1")
+  .text("United States GDP, 1947-2015")
+  .attr("id", "title");
+
 fetch(
   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json"
 )
