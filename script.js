8706832f97cbddf90feda52046e109c82a4a3cf9
r-s-i <77388911+r-s-i@users.noreply.github.com>
2023-02-09 09:28:58 +0100

    Added barebone fetch method.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+fetch(
+  "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json"
+)
+  .then((r) => r.json())
+  .then((d) => {
+    console.log(d);
+    // Main code goes here
+  })
+  .catch((e) => console.error(e));
